593b5f86257d969b400d9f2353eb42ef05a49155
yhu04 <constance.woo58@gmail.com>
2016-06-20 23:08:23 -0400

    Update reverse_integer.py

--- reverse_integer.py
+++ reverse_integer.py
@@ -6,4 +6,19 @@ class Solution(object):
         """
         
         if x>=0:
+            sign=1
+        else:
+            sigh=-1
+        
+        reverse=0
+        temp=abs(x)
+        while temp:
+            reverse = reverse*10+temp%10
+            reverse /= 10
+        
+        if reverse>2^32:
+            return 0
+        else:
+            return reverse*sign
+        
         
