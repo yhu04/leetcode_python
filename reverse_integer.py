2677d616a909e63f8a5ff3e98691f00f4fe241bc
yhu04 <constance.woo58@gmail.com>
2016-06-20 23:14:48 -0400

    Update reverse_integer.py

--- reverse_integer.py
+++ reverse_integer.py
@@ -4,21 +4,18 @@ class Solution(object):
         :type x: int
         :rtype: int
         """
-        
         if x>=0:
             sign=1
         else:
-            sigh=-1
+            sign=-1
         
+        x=abs(x)
         reverse=0
-        temp=abs(x)
-        while temp:
-            reverse = reverse*10+temp%10
-            reverse /= 10
+        while x>0:
+            reverse=reverse*10+x%10
+            x=x/10
         
-        if reverse>2^32:
+        reverse=reverse*sign
+        if reverse < -(1 << 31) or reverse > (1 << 31) - 1:
             return 0
-        else:
-            return reverse*sign
-        
-        
+        return reverse
