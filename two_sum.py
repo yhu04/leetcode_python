be842210de90defb41f434d471622714a64db1c3
yhu04 <constance.woo58@gmail.com>
2016-06-20 21:33:12 -0400

    Update two_sum.py

--- two_sum.py
+++ two_sum.py
@@ -1,3 +1,19 @@
+
+#Brute Force n^2
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i]+nums[j]==target:
+                    return[i,j]
+        return [-1,-1]
+
+# n 
 class Solution(object):
     def twoSum(self, nums, target):
         """
@@ -8,4 +24,9 @@ class Solution(object):
         # create a dictionary for the list and its indice
         nums_dict = {}
         for i in range(len(nums)):
+            if target-nums[i] in nums_dict:
+                return [nums_dict[target-nums[i]],i]
+            nums_dict[nums[i]]=i
+        # no result
+        return [-1,-1]
   
